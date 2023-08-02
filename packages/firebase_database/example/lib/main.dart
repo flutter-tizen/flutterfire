0651ebfa31d8c565eeb04a2dbb2c401c63b394d0
Daeyeon Jeong <daeyeon.jeong@samsung.com>
2023-08-02 13:57:42 +0900

    fixup! fixup! fixup! feat(database): add initial firebase database
    Signed-off-by: Daeyeon Jeong <daeyeon.jeong@samsung.com>

--- packages/firebase_database/example/lib/main.dart
+++ packages/firebase_database/example/lib/main.dart
@@ -1,140 +1,240 @@
-// Copyright 2022, the Chromium project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-import 'dart:io';
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb;
 import 'package:flutter/material.dart';
+
 import 'firebase_options.dart';
+
+// Change to false to use live database instance.
+const USE_DATABASE_EMULATOR = true;
+// The port we've set the Firebase Database emulator to run on via the
+// `firebase.json` configuration file.
+const emulatorPort = 9000;
+// Android device emulators consider localhost of the host machine as 10.0.2.2
+// so let's use that if running on Android.
+final emulatorHost =
+    (!kIsWeb && defaultTargetPlatform == TargetPlatform.android)
+        ? '10.0.2.2'
+        : 'localhost';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  if (USE_DATABASE_EMULATOR) {
+    FirebaseDatabase.instance.useDatabaseEmulator(emulatorHost, emulatorPort);
   }
+
+  runApp(
+    const MaterialApp(
+      title: 'Flutter Database Example',
+      home: MyHomePage(),
+    ),
+  );
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  late DatabaseReference _counterRef;
+  late DatabaseReference _messagesRef;
+  late StreamSubscription<DatabaseEvent> _counterSubscription;
+  late StreamSubscription<DatabaseEvent> _messagesSubscription;
+  bool _anchorToBottom = false;
 
-  void _incrementCounter() {
+  String _kTestKey = 'Hello';
+  String _kTestValue = 'world!';
+  FirebaseException? _error;
+  bool initialized = false;
+
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
+
+  Future<void> init() async {
+    _counterRef = FirebaseDatabase.instance.ref('counter');
+
+    final database = FirebaseDatabase.instance;
+
+    _messagesRef = database.ref('messages');
+
+    database.setLoggingEnabled(false);
+
+    if (!kIsWeb) {
+      database.setPersistenceEnabled(true);
+      database.setPersistenceCacheSizeBytes(10000000);
+    }
+
+    if (!kIsWeb) {
+      await _counterRef.keepSynced(true);
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      initialized = true;
+    });
+
+    try {
+      final counterSnapshot = await _counterRef.get();
+
+      print(
+        'Connected to directly configured database and read '
+        '${counterSnapshot.value}',
+      );
+    } catch (err) {
+      print(err);
+    }
+
+    _counterSubscription = _counterRef.onValue.listen(
+      (DatabaseEvent event) {
+        setState(() {
+          _error = null;
+          _counter = (event.snapshot.value ?? 0) as int;
+        });
+      },
+      onError: (Object o) {
+        final error = o as FirebaseException;
+        setState(() {
+          _error = error;
+        });
+      },
+    );
+
+    final messagesQuery = _messagesRef.limitToLast(10);
+
+    _messagesSubscription = messagesQuery.onChildAdded.listen(
+      (DatabaseEvent event) {
+        print('Child added: ${event.snapshot.value}');
+      },
+      onError: (Object o) {
+        final error = o as FirebaseException;
+        print('Error: ${error.code} ${error.message}');
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _messagesSubscription.cancel();
+    _counterSubscription.cancel();
+  }
+
+  Future<void> _increment() async {
+    await _counterRef.set(ServerValue.increment(1));
+
+    await _messagesRef
+        .push()
+        .set(<String, String>{_kTestKey: '$_kTestValue $_counter'});
+  }
+
+  Future<void> _incrementAsTransaction() async {
+    try {
+      final transactionResult = await _counterRef.runTransaction((mutableData) {
+        return Transaction.success((mutableData as int? ?? 0) + 1);
+      });
+
+      if (transactionResult.committed) {
+        final newMessageRef = _messagesRef.push();
+        await newMessageRef.set(<String, String>{
+          _kTestKey: '$_kTestValue ${transactionResult.snapshot.value}'
+        });
+      }
+    } on FirebaseException catch (e) {
+      print(e.message);
+    }
+  }
+
+  Future<void> _deleteMessage(DataSnapshot snapshot) async {
+    final messageRef = _messagesRef.child(snapshot.key!);
+    await messageRef.remove();
+  }
+
+  void _setAnchorToBottom(bool? value) {
+    if (value == null) {
+      return;
+    }
+
+    setState(() {
+      _anchorToBottom = value;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    if (!initialized) return Container();
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Flutter Database Example'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () async {
-                // Running these APIs manually as they're failing on CI due to required keychain sharing entitlements
-                // See this issue https://github.com/firebase/flutterfire/issues/9538
-                // You will also need to add the keychain sharing entitlements to this test app and sign code with development team for app & tests to successfully run
-                if (Platform.isMacOS && kDebugMode) {
-                  // ignore_for_file: avoid_print
-                  // Wait a little so we don't get a delete-pending exception
-                  await Future.delayed(const Duration(seconds: 8));
-                }
+      body: Column(
+        children: [
+          Flexible(
+            child: Center(
+              child: _error == null
+                  ? Text(
+                      'Button tapped $_counter time${_counter == 1 ? '' : 's'}.\n\n'
+                      'This includes all devices, ever.',
+                    )
+                  : Text(
+                      'Error retrieving button tap count:\n${_error!.message}',
+                    ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: _incrementAsTransaction,
+            child: const Text('Increment as transaction'),
+          ),
+          ListTile(
+            leading: Checkbox(
+              onChanged: _setAnchorToBottom,
+              value: _anchorToBottom,
+            ),
+            title: const Text('Anchor to bottom'),
+          ),
+          Flexible(
+            child: FirebaseAnimatedList(
+              key: ValueKey<bool>(_anchorToBottom),
+              query: _messagesRef,
+              reverse: _anchorToBottom,
+              itemBuilder: (context, snapshot, animation, index) {
+                return SizeTransition(
+                  sizeFactor: animation,
+                  child: ListTile(
+                    trailing: IconButton(
+                      onPressed: () => _deleteMessage(snapshot),
+                      icon: const Icon(Icons.delete),
+                    ),
+                    title: Text('$index: ${snapshot.value.toString()}'),
+                  ),
+                );
               },
-              child: const Text('Test macOS tests manually'),
             ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _increment,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
